55bc9a53139b242f0b01d7455cae516f67c110c7
SwapnaChidananda <148573365+SwapnaChidananda@users.noreply.github.com>
2023-10-20 12:57:13 -0400

    Update test_calculators.py

--- src/test_calculators.py
+++ src/test_calculators.py
@@ -36,5 +36,12 @@ class TestMultiCalc(unittest.TestCase):
         error_message = app.calculate_currency('invalid', '($)', '($)')
         self.assertEqual(error_message, "Invalid input")
 
+    def test_functionality_area_rectangle(self):
+        app = MultiCalc()
+        length = 5
+        width = 10
+        area_result = app.calculate_area('rectangle', length, width)
+        self.assertEqual(area_result, "Area: 50.0 square units")
+
 if __name__ == "__main__":
     unittest.main()
